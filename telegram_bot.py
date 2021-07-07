e1d16b798495010c0a79a138b6dc9f86082cad58
voven4ek <batsura.vs@gmail.com>
2021-07-07 18:43:56 +0300

    1

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,12 @@
 import os
-
+import requests
 from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler
 from main import MakeVideos
 
 token = "1833415580:AAFeqCeMjhSdlGbNV7BgtSGBYsAV5mYBaQA"
 bot = Bot(token)
+images = ['png', 'jpg', 'jpeg']
 
 
 def make_videos(update, context):
@@ -32,8 +33,30 @@ def start(update, context):
     Для лучшего результата используйте картинки 16:9''')
 
 
+def from_url(update, context):
+    command = update.message.text
+    if len(command.split(' ')) == 2 and command.split(' ')[1].split('?')[0].split('.')[-1] in images:
+        url = command.split(' ')[1]
+        data = requests.get(url)
+        if data.status_code == 200:
+            filename = f'{update.message.from_user.id}.{command.split(" ")[1].split("?")[0].split(".")[-1]}'
+            with open(filename, 'wb') as f:
+                f.write(data.content)
+            vid = MakeVideos(filename)
+            files = vid.create()
+            z = 3
+            for i in files:
+                bot.send_video(update.message.chat_id, open(i, 'rb'), caption=f'Выкладывать {z}')
+                z -= 1
+                os.remove(i)
+            os.remove(filename)
+            return 0
+    update.message.reply_text('По ссылке не находится изображение! Либо невозможно определить его тип!')
+
+
 if __name__ == '__main__':
     updater = Updater(token)
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, make_videos, run_async=True))
     updater.dispatcher.add_handler(CommandHandler('start', start, run_async=True))
+    updater.dispatcher.add_handler(CommandHandler('from_url', from_url, run_async=True))
     updater.start_polling()
